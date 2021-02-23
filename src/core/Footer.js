e11feab156d2a07e99168b450becff379cf20a29
sajidkhak <sajidkhaki@gmail.com>
2021-02-23 21:02:17 +0530

    footer fixed

--- src/core/Footer.js
+++ src/core/Footer.js
@@ -4,9 +4,11 @@ import React from 'react';
 const Footer = () => {
 
     return (
-        <div className="jumbotron-footer mt-5 pt-5">
-            <h2>Book Store</h2>
-            <p className="lead">You can now purchase your favourite books, from you favourite seller</p>
+        <div className="footer mt-5 pt-5">
+            <h2>About Me</h2>
+            <p className="lead text-warning h6">I Am Experienced Fullstack --MERN Developer.
+                Microsoft Student Partner And Windows U-Crew Member</p>
+            <span>Vist Me: <a className="text-warning" href="https://sajidkhaki.sajidkhaki.vercel.app" >Sajid khaki</a></span>
         </div>
     )
 }
